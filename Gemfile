e6401020d5453fc1ef28608c6635d3b5e232bada
Harvin <Harvin@Harvins-MacBook-Pro.local>
2015-12-10 17:52:33 -0600

    Setup database.yml

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'puma'
 gem 'foreman'
 gem 'devise'
+gem 'pg'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,8 @@
 class API::V1::BaseController < ApplicationController
 	protect_from_forgery with: :null_session
-	before_action	:destroy_session
+	before_action :destroy_session
+
 	def destroy_session
-		request.session_options[:skip] = true
+	    request.session_options[:skip] = true
 	end
 end
