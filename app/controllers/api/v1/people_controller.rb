ca00000bbf29055d6d980ab8ee190472d31a0246
Harvin <Harvin@Harvins-MacBook-Pro.local>
2015-12-11 17:15:51 -0600

    Add session controller

--- app/controllers/api/v1/people_controller.rb
+++
@@ -1,8 +0,0 @@
-class API::V1::PeopleController < API::V1::BaseController
-  def index
-     @people=[{first_name:"Harvin",last_name: "LLanes"}]
-     respond_to do |format|
-       format.json {render :json => @people }
-     end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-        resources :people
         resources :users ,:only =>[:index,:show,:create]
+        resources :sessions, :only =>[:create,:destroy]
     end
   end
 end
