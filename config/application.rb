950bb5e9a9d1879987e01d8648b33d8c1638dbbf
Harvin <Harvin@Harvins-MacBook-Pro.local>
2015-12-11 11:54:44 -0600

    Create rspec_model

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module Perception
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.generators do |g|
+      g.factory_girl true
+      g.factory_girl dir: 'spec/factories'
+    end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
