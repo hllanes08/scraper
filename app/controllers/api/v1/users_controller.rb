1d2257f8fc6e2b8f90ccae8e944f535c43ce8b4e
Harvin Llanes <Harvin@Harvins-Mac-mini.local>
2015-12-27 11:57:00 -0600

    Hide location for filter

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ class API::V1::UsersController < API::V1::BaseController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: 201,location: [:api,user]
+      render json: user, status: 201#,location: [:api,user]
     else
       render json: {errors: user.errors },status:422
     end
@@ -20,7 +20,7 @@ class API::V1::UsersController < API::V1::BaseController
       user = current_user
 
       if user.update(user_params)
-        render json: user, status: 200, location: [:api, user]
+        render json: user, status: 200#, location: [:api, user]
       else
         render json: { errors: user.errors }, status: 422
       end
