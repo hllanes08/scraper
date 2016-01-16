4b95248a9a8f46c29b9f0d485724902bf7eab92f
Harvin Llanes <harvinllanes@moi-solutions.com>
2016-01-15 22:37:27 -0600

    Modify migrations

--- db/migrate/20160116043420_create_items.rb
+++ db/migrate/20160116043420_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.integer :id
       t.string :type
       t.string :name
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211213929) do
+ActiveRecord::Schema.define(version: 20160116043420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "type"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
